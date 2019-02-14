78dcf81ad8a33d105bcddeb413d44e212f629565
NAPIER-MAIL\40204497 <40204497@live.napier.ac.uk>
2019-02-14 12:33:27 +0000

    Need to continue writting ecm library

--- lib_ecm/ecm.cpp
+++ lib_ecm/ecm.cpp
@@ -3,23 +3,49 @@
 using namespace std;
 using namespace sf;
 
-const Vector2f Entity::getPosition() { return _position + Vector2f(0.0f, 30.0f); }
+Entity::Entity() : _position({ 0.0f,0.0f }), _rotation(0.0f), _alive(true), _visible(true),_fordeletion(false){}
+
+const Vector2f& Entity::getPosition() const { return _position + Vector2f(0.0f, 30.0f); }
 
 void Entity::setPosition(const Vector2f &pos) { _position = pos; }
 
-void Entity::move(const Vector2f &pos) { _position += pos; }
+//void Entity::move(const Vector2f &pos) { _position += pos; }
 
 void Entity::update(const double dt) {
-	_shape->setPosition(_position);
+	//_shape->setPosition(_position);
+	for (auto c : _components) {
+		c->update(dt);
+	}
 }
 
-
-Entity::Entity(unique_ptr<Shape> s) : _shape(std::move(s)) {}
+bool Entity::is_fordeletion() const { return _fordeletion; }
+//Entity::Entity(unique_ptr<Shape> s) : _shape(std::move(s)) {}
 
 
 void Entity::render() {
 //	Renderer::queue(_shape.get());	//Removed this as its specific to Practical 4
+	for (auto c : _components) {
+		c->render();
+	}
 }
+
+float Entity::getRotation() const { return _rotation; }
+
+void Entity::setRotation(float rotation) { _rotation = rotation; }
+
+bool Entity::isAlive() const { return _alive; }
+
+void Entity::setAlive(bool alive) { _alive = alive; }
+
+void Entity::setForDelete() { 
+	_fordeletion = true;
+	_alive = false;
+	_visible = false;
+}
+
+bool Entity::isVisible() const { return _visible; }
+
+void Entity::setVisible(bool visible) { _visible = visible; }
 
 void EntityManager::render() {	//was working
 	for (auto &e : list) {
@@ -34,3 +60,7 @@ void EntityManager::update(double dt) {
 		e->update(dt);
 	}
 }
+
+Component::Component(Entity *const p) : _parent(p), _fordeletion(false) {}
+
+Entity::~Entity(){}
--- lib_ecm/ecm.h
+++ lib_ecm/ecm.h
@@ -2,34 +2,44 @@
 
 #include <SFML/Graphics.hpp>
 //#include "system_renderer.h"	//Specific to practical 4 until Checkpoint 1
+
+
+
+
+
 #include "maths.h"
 #include <algorithm>
 #include <memory>
 #include <typeindex>
 #include <vector>
 
+class Component; //forward declare
+
 class Entity {
+
 protected:
-	std::unique_ptr<sf::Shape> _shape;
+	std::vector<std::shared_ptr<Component>> _components;
 	sf::Vector2f _position;
-	Entity(std::unique_ptr<sf::Shape> shp);
-
+	float _rotation;
+	bool _alive;       // should be updated
+	bool _visible;     // should be rendered
+	bool _fordeletion; // should be deleted
 public:
-	Entity() = delete;
-	virtual ~Entity() = default;
+	Entity();
+	virtual ~Entity();
+	virtual void update(double dt);
+	virtual void render();
 
-	virtual void update(const double dt);
-	virtual void render(sf::RenderWindow &window) const = 0;
-
-	void render();
-
-	//virtual void render() const;
-
-
-
-	const sf::Vector2f getPosition();
-	void setPosition(const sf::Vector2f &pos);
-	void move(const sf::Vector2f &pos);
+	const sf::Vector2f& getPosition() const;
+	void setPosition(const sf::Vector2f &_position);
+	bool is_fordeletion() const;
+	float getRotation() const;
+	void setRotation(float _rotation);
+	bool isAlive() const;
+	void setAlive(bool _alive);
+	void setForDelete();
+	bool isVisible() const;
+	void setVisible(bool _visible);
 };
 
 
@@ -38,3 +48,91 @@ struct EntityManager {
 	void update(double dt);
 	void render();
 };
+
+
+class Component {
+protected:
+	Entity *const _parent;
+	bool _fordeletion; // should be removed
+	explicit Component(Entity *const p);
+
+public:
+	Component() = delete;
+	bool is_fordeletion() const;
+	virtual void update(double dt) = 0;
+	virtual void render() = 0;
+	virtual ~Component();
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+//#include "maths.h"
+//#include <algorithm>
+//#include <memory>
+//#include <typeindex>
+//#include <vector>
+//
+//class Component; //forward declare
+//
+//
+//class Entity {
+//protected:
+//	std::unique_ptr<sf::Shape> _shape;
+//	sf::Vector2f _position;
+//	Entity(std::unique_ptr<sf::Shape> shp);
+//
+//public:
+//	Entity() = delete;
+//	virtual ~Entity() = default;
+//
+//	virtual void update(const double dt);
+//	virtual void render(sf::RenderWindow &window) const = 0;
+//
+//	void render();
+//
+//	//virtual void render() const;
+//
+//
+//
+//	const sf::Vector2f getPosition();
+//	void setPosition(const sf::Vector2f &pos);
+//	void move(const sf::Vector2f &pos);
+//};
+//
+//
+//struct EntityManager {
+//	std::vector<std::shared_ptr<Entity>> list;
+//	void update(double dt);
+//	void render();
+//};
